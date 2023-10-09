2378fa06cfa4438ccb5e9eb60306c3a8e0d17337
杜乾坤 <3441423164@qq.com>
2023-10-09 17:16:30 +0800

    vue3

---
+++ vue/1_vue/vue3-study/src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+  import hello from './components/hello.vue'
+  import demo from './components/demo.vue'
+</script>
+<template>
+  <hello />
+  <demo />
+</template>
---
+++ vue/1_vue/vue3-study/src/components/hello.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<template>
+    <div id="app">
+        <h1>hello world!!</h1>
+        <button @click="count++">Count is: {{ count }}</button>
+    </div>
+</template>
+
+<style scoped>
+button {
+    font-weight: bold;
+}
+</style>
