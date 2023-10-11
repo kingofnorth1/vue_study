a05f72cf5a4bf41b45950a3d4be6c7103920551f
kingofnorth1 <3441423164@qq.com>
2023-10-12 00:26:01 +0800

    23/10/12

--- vue/vue3/vue3-login-demo/src/App.vue
+++ vue/vue3/vue3-login-demo/src/App.vue
@@ -1,9 +1,24 @@
 <script setup>
+import {ref,reactive} from 'vue'
 import login from './components/login.vue';
+const message = ref()
+// let hours = reactive()
+// let minutess = reactive()
+// let secondss = reactive()
+function callback(val) {
+  console.log("父类时间："+val);
+  console.log(typeof(val.toString()))
+  message.value = val;
+  // hours = hour;
+  // minutess = minutes;
+  // secondss = seconds;
+}
 </script>
 
 <template>
   <div>
-    <h3>demo</h3>
+    <!-- <h3>子类时间:{{ hours }}  {{ minutess }}  {{ secondss }}</h3> -->
+    <h3>子类时间：{{ message }}</h3>
+    <h3>子類: <login @some-event="callback"/></h3>
   </div>
 </template>
--- vue/vue3/vue3-login-demo/src/components/login.vue
+++ vue/vue3/vue3-login-demo/src/components/login.vue
@@ -1,7 +1,22 @@
 <script setup>
+import {ref} from "vue"
+const emit = defineEmits(['someEvent'])
 
+function getTime(){
+    const now = new Date();
+    // const year = new Date().getFullYear();
+    // const month = new Date().getMonth();
+    // const day = new Date().getDay();
+    const hour = new Date().getHours().toString();
+    const minutes = new Date().getMinutes().toString();
+    const seconds = new Date().getSeconds().toString();
+    // emit('someEvent', hour, minutes, seconds)
+    emit('someEvent', now)
+}
 </script>
 
 <template>
-  
+  <div>
+    <button @click="getTime">click me</button>
+  </div>
 </template>
