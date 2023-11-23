99bd8a23006e4aaafbc774ba31e09b019bd00b30
杜乾坤 <3441423164@qq.com>
2023-11-23 11:08:30 +0800

    javaScript Study

--- vue/javaScript/Study/hello.js
+++ vue/javaScript/Study/hello.js
@@ -3,6 +3,17 @@ const PI = 3.14
 for (let i=0;i<5;i++){
     console.log("hello, World!")
 }
+//字符串拼接
+var username = "John"
+var age = 18
+console.log("Hello EveryOne My name is "+username+" My age is "+age)
+console.log(`Hello EveryOne My name is ${username} My age is ${age}`)
+//字符串内置方法
+const s = "Hello, Wrold"
+console.log(s.toUpperCase())
+console.log(s.toLowerCase())
+console.log(s.substring(0,6))
+console.log(s.split(", "))
 //函数return返回
 function f(){
     console.log(PI)
@@ -15,7 +26,7 @@ console.log(f());
 let str = "nkmYM3441423164@qq.com".replace("3441423164@qq.com", '')
 console.log(str)
 //遍历链表打印
-let arr = [2,5,8,5,9,0]
+let arr = [2,5,0]
 for (let i in arr){
     console.log(arr[i])
 }
@@ -30,4 +41,43 @@ function* helloWorldGenerator() {
 }
 var hw = helloWorldGenerator();
 console.log(hw)
-  
+//对象
+const person = {
+    username: "sun",
+    age: "33",
+    sex: "man"
+}
+var { username, age, sex } = person
+console.log(person.username)
+console.log(username, age, sex)
+person.email = "3441423164@qq.com"
+console.log(person.email)
+//javaScript 数组与JSOn
+const todos = [
+    {
+        id: 1,
+        text: "Take out trash ",
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: "Meeting with boss",
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: "Dentist appt",
+        isCompleted: false
+    }
+]
+console.log(todos[1].text)
+const todoJson = JSON.stringify(todos)
+console.log(todoJson)
+console.log(1 > 10 ? "yes":"no")
+//基础语法
+let color = "3"
+switch (color) {
+    case "1": console.log("red"); break;
+    case "2": console.log("blue"); break;
+    default: console.log("rainbow");
+}
